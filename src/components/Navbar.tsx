f5e5b69bbe4d4a194cda6fd8ca12c1718d18f242
robertjohnson1270001-arch <robertjohnson127.0.0.0.1@gmail.com>
2026-03-09 16:49:56 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
     >
       <div className="container mx-auto flex items-center justify-between h-16 md:h-20 px-4">
         <a href="#home">
-          <img src={logo} alt="NxtEra Media" className="h-35 md:h-29 w-auto" />
+          <img src={logo} alt="NxtEra Media" className="h-35 md:h-26 w-auto" />
         </a>
 
         {/* Desktop */}
